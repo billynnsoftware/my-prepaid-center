18d0a631ea8fe57f7ee9a1bbbf8228c41e24c2e2
billynnsoftware <contact.nnsoftware@gmail.com>
2024-05-14 16:12:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,6 @@
  * @package WordPress
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
